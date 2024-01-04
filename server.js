15591e3bbd1dbcd063f89a6ff40f3523b7c5ec58
ehmed007 <ehmedmughal@gmail.com>
2024-01-04 18:10:39 +0500

    okay

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http1 = require("http")
 const app = require("./app")
 require('dotenv').config()
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; 
 const server = http1.createServer(app);   
 
 server.listen(port,(response) => { 
